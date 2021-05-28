322db13b19d94907f077d4566f3f283dbbb564af
Neha Choudhary <Neha_Choudhary@epam.com>
2021-05-28 15:29:51 +0530

    pact TC

--- src/main/java/com/example/demo/client/PetClient.java
+++ src/main/java/com/example/demo/client/PetClient.java
@@ -1,12 +1,28 @@
 package com.example.demo.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.resource.Pet;
 
 @FeignClient(name = "pet-service-client", url = "${pet.service.prefix.url}")
 public interface PetClient {
 
 	@GetMapping("/pets")
-	String getPetDetails();
+	Pet getPetDetails();
+
+	@PostMapping("/pets")
+	void savePet(@RequestBody Pet pet);
+
+	@PutMapping("/pets/{name}")
+	Pet updatePet(@PathVariable String name, @RequestBody Pet pet);
+
+	@DeleteMapping("/pets/{name}")
+	void deletePet(@PathVariable String name);
 
 }
--- src/test/java/com/example/demo/resource/AnimalServiceTest.java
+++ src/test/java/com/example/demo/resource/AnimalServiceTest.java
@@ -2,6 +2,9 @@ package com.example.demo.resource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.example.demo.client.PetClient;
 
 import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
@@ -28,14 +32,67 @@ class AnimalServiceTest {
 	@Test
 	@PactTestFor(pactMethod = "pactPetExists")
 	void getAnimal() {
-		String pet = petClient.getPetDetails();
-		assertEquals("dog", pet);
+		Pet pet = petClient.getPetDetails();
+		assertEquals("Bruno", pet.getName());
+	}
+
+	@Test
+	@PactTestFor(pactMethod = "pactSavePet")
+	void saveAnimal() {
+		Pet pet = new Pet("Bruno", "Dog");
+		petClient.savePet(pet);
+	}
+
+	@Test
+	@PactTestFor(pactMethod = "pactUpdatePet")
+	void updateAnimal() {
+		Pet pet = new Pet("Bruno", "Cat");
+		petClient.updatePet("Bruno", pet);
+		assertEquals("Bruno", pet.getName());
+	}
+
+	@Test
+	@PactTestFor(pactMethod = "pactDeletePet")
+	void deleteAnimal() {
+		petClient.deletePet("Bruno");
 	}
 
 	@Pact(provider = "pet-service", consumer = "petClient")
 	public RequestResponsePact pactPetExists(PactDslWithProvider builder) {
+		Map<String, String> headers = new HashMap();
+		headers.put("Content-Type", "application/json");
 		return builder.given("Pet exists").uponReceiving("A request to /pets").path("/pets").method("GET")
-				.willRespondWith().status(200).body("dog").toPact();
+				.willRespondWith().status(200).headers(headers).body(new PactDslJsonBody()
+						.stringMatcher("name", "[A-Za-z]*", "Bruno").stringMatcher("type", "[A-Za-z]*", "Dog"))
+				.toPact();
 	}
 
+	@Pact(provider = "pet-service", consumer = "petClient")
+	public RequestResponsePact pactSavePet(PactDslWithProvider builder) {
+		Map<String, String> headers = new HashMap();
+		headers.put("Content-Type", "application/json");
+		return builder.given("Create a new pet").uponReceiving("A request to /pets").path("/pets").headers(headers)
+				.method("POST").body(new PactDslJsonBody().stringMatcher("name", "[A-Za-z]*", "Bruno")
+						.stringMatcher("type", "[A-Za-z]*", "Dog"))
+				.willRespondWith().status(200).toPact();
+	}
+
+	@Pact(provider = "pet-service", consumer = "petClient")
+	public RequestResponsePact pactUpdatePet(PactDslWithProvider builder) {
+		Map<String, String> headers = new HashMap();
+		headers.put("Content-Type", "application/json");
+		return builder.given("Update a new pet").uponReceiving("A request to /pets").matchPath("/pets/[A-Za-z]+")
+				.headers(headers).method("PUT")
+				.body(new PactDslJsonBody().stringMatcher("name", "[A-Za-z]*", "Bruno").stringMatcher("type",
+						"[A-Za-z]*", "Dog"))
+				.willRespondWith().status(200).headers(headers).body(new PactDslJsonBody()
+						.stringMatcher("name", "[A-Za-z]*", "Bruno").stringMatcher("type", "[A-Za-z]*", "Dog"))
+				.toPact();
+	}
+
+	@Pact(provider = "pet-service", consumer = "petClient")
+	public RequestResponsePact pactDeletePet(PactDslWithProvider builder) {
+		return builder.given("delete a new pet").uponReceiving("A request to /pets").matchPath("/pets/[A-Za-z]+")
+				.method("DELETE").willRespondWith().status(200).toPact();
+	}
 }
